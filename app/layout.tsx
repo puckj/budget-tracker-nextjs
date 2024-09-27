ca5516a73ffa506681d70d48baa91ec7bd887e7e
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2024-09-27 16:53:03 +0700

    NavBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,13 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className="dark" style={{ colorScheme: 'dark' }}>
+      <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <RootProviders>
-            {children}
-          </RootProviders>
+          <RootProviders>{children}</RootProviders>
         </body>
       </html>
     </ClerkProvider>
